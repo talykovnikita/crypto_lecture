3569a4bbfdcf45970758e98e55a1882ffe1599e4
Talykov Nikita <talykovnikita@gmail.com>
2023-12-25 23:10:28 +0300

    добавил тесты

--- one_time_password/tests.py
+++ one_time_password/tests.py
@@ -1,6 +1,15 @@
 from one_time_pad_common import crypt
 
 
+def test_0():
+    msg: str = "HELLOWORLD"
+    key: str = "helloworld"
+
+    result_1 = crypt(msg, key)
+
+    assert result_1 != msg, f"Нулевой тест не пройден. Функция crypt ничего не делает."
+
+
 def test_1():
     msg: str = "HELLOWORLD"
     key: str = "helloworld"
@@ -37,7 +46,7 @@ def test_4():
     assert msg == result, f"Четвёртый тест не пройден. Ожидали {msg}, а получили {result}"
 
 
-TESTS = [test_1, test_2, test_3, test_4]
+TESTS = [test_0, test_1, test_2, test_3, test_4]
 
 
 def main():
